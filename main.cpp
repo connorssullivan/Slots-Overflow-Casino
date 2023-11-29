d01245807e0b7344463a443786150d95ea9677e7
connorsullivan <connorssullivan25@gmail.com>
2023-11-28 19:41:09 -0500

    Final Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,67 @@
 #include "Card.h"
 #include "Poker.h"
 #include "Blackjack.h"
+#include <fstream>
 
 int main(){
-    int playerBalance = 1000;
+    ifstream input; //Set up if stream
+    char name[50]; //Player name
+    char password[50]; //Player password
+    int playerBalance;
+
+    bool nameFound = false;
+    
+    //Open File
+    input.open("login.txt", ios::in);
+    if(!input){//On error
+        cerr << "\nNo file found" <<endl;
+        exit(1);
+    }
+
+    //Get users name and password
+    while(!nameFound){
+        cout << "\nEnter Your Name: ";
+        char enteredName[50];
+        while (!(cin >> enteredName)){//Validate name
+            cin.clear();
+            cin.ignore();
+        }
+
+        //Search through the file
+        while(!input.eof()){
+            while(input >> name >> password >> playerBalance){
+                if(strcmp(name, enteredName) == 0){
+                    nameFound = true;
+                    break;
+                }
+            }
+        }
+
+        //If name not found
+        if(!nameFound && input.eof()){
+            cout << "\nSorry Thats not in our system, Please Talk to the cashiers office to set up an account" << endl;
+            input.close(); //Close file
+            return -1;
+        }
+
+    }
+   
+    //Get password
+    char enteredPasword[50];
+    cout << "Enter password:";
+    while (!(cin >> enteredPasword)){//Validate password
+            cin.clear();
+            cin.ignore();
+    }
+
+    //Check password
+    if(strcmp(password, enteredPasword) != 0){
+        cout << "\nSorry Thats not the correct password, Please Talk to the cashiers office to retreive your password" << endl;
+        input.close();
+        return -1;
+    }
+
+    //Main loop
     while(true){
         int mainMenuOption = -1;
 
@@ -51,8 +109,11 @@ int main(){
         }
     }
 
-    
-    
+    ofstream output("temp.txt", ios::out);
+   output << " "<< name << " "<< password << " " << playerBalance;
 
+   input.close();
+   output.close();
+    
     return 0;
 }
